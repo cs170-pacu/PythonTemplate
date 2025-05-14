7c293811c0ec3814089bc03f291051950a050841
chadd williams <will4614@pacificu.edu>
2025-05-14 10:36:57 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,16 @@
+################################################################################
+# File name:    main.py
+# Author:       YOUR NAME HERE
+# Date:         5/14/2025
+# Class:        CS 170
+# Assignment:   01Lab
+# Purpose:      Demonstrate Codespaces & Python
+# Hours:        1.5
+################################################################################
+
 import sys
 
-print('hello!', sys.argv[0], sys.argv[1])
+print('hello!', sys.argv[0])
+
+if len(sys.argv) > 1:
+    print(sys.argv[1])
